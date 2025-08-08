6efc08d6ced15cf613d3866471ec3f44fa1a5e5c
Kelvin Jesus Avelino Cambiaghi <kj.avelino.cambiaghi@gmail.com>
2025-08-07 21:16:55 -0300

    feat: ignoring comments and whitespaces

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -11,7 +11,7 @@ var current int = 0
 
 // Keeps tracking of the current line
 // of source code
-var currentLine int = 1
+var currentLine uint = 1
 
 type Lexer struct {
 	Tokens []token.Token
@@ -28,16 +28,47 @@ func (lexer *Lexer) Tokenize() {
 
 		var character rune = lexer.advance()
 
-		if character == '+' {
+		switch character {
+		case '\n':
+			currentLine++
+		case '\t', '\r':
+			continue
+		case '/':
+			// it's a comment
+			if lexer.peek() == '/' {
+				// consumes tokens until it reaches \n
+				for lexer.peek() != '\n' {
+					lexer.advance()
+				}
+			} else {
+				lexer.addToken(token.TOK_SLASH)
+			}
+		case '(':
+			lexer.addToken(token.TOK_LEFT_PAREN)
+		case ')':
+			lexer.addToken(token.TOK_RIGHT_PAREN)
+		case '[':
+			lexer.addToken(token.TOK_LEFT_SQR_BCKT)
+		case ']':
+			lexer.addToken(token.TOK_RIGHT_SQR_BCKT)
+		case '.':
+			lexer.addToken(token.TOK_DOT)
+		case ',':
+			lexer.addToken(token.TOK_COMMA)
+		case '+':
 			lexer.addToken(token.TOK_PLUS_SIGN)
-		}
-
-		if character == '-' {
+		case '-':
 			lexer.addToken(token.TOK_MINUS_SIGN)
-		}
-
-		if character == '*' {
+		case '*':
 			lexer.addToken(token.TOK_STAR_SIGN)
+		case '^':
+			lexer.addToken(token.TOK_CARET)
+		case ';':
+			lexer.addToken(token.TOK_SEMICOLON)
+		case '?':
+			lexer.addToken(token.TOK_QUESTION)
+		case '%':
+			lexer.addToken(token.TOK_MOD_SIGN)
 		}
 
 	}
@@ -59,6 +90,34 @@ func (lexer *Lexer) addToken(tokenType token.TokenType) {
 		token.Token{
 			Type:   tokenType,
 			Lexeme: lexer.Source[start:current],
+			Line:   currentLine,
 		},
 	)
+}
+
+// Takes a peek at the current character but dsn't consume it
+func (lexer *Lexer) peek() rune {
+	return lexer.Source[current]
+}
+
+// Takes a peek the character at current + *position*
+// without consuming it
+func (lexer *Lexer) lookAhead(position int) rune {
+	return lexer.Source[current+position]
+}
+
+// Check if the character at current position matches
+// the *expected* and if so, consumes the character
+func (lexer *Lexer) match(expected rune) bool {
+	if current >= len(lexer.Source) {
+		return false
+	}
+
+	if lexer.Source[current] != expected {
+		return false
+	}
+
+	current++
+
+	return true
 }
--- internal/token/token.go
+++ internal/token/token.go
@@ -7,21 +7,31 @@ type TokenType string
 // Constants for token types
 const (
 	// Single-char tokens
-	TOK_LEFT_PAREN  TokenType = "TOK_LEFT_PAREN"  // (
-	TOK_RIGHT_PAREN TokenType = "TOK_RIGHT_PAREN" // )
-	TOK_LEFT_CURLY  TokenType = "TOK_LEFT_CURLY"  // {
-	TOK_RIGTH_CURLY TokenType = "TOK_RIGTH_CURLY" // }
-	TOK_PLUS_SIGN   TokenType = "TOK_PLUS_SIGN"   // +
-	TOK_MINUS_SIGN  TokenType = "TOK_MINUS_SIGN"  // -
-	TOK_STAR_SIGN   TokenType = "TOK_STAR_SIGN"   // *
-
+	TOK_LEFT_PAREN     TokenType = "TOK_LEFT_PAREN"     // (
+	TOK_RIGHT_PAREN    TokenType = "TOK_RIGHT_PAREN"    // )
+	TOK_LEFT_CURLY     TokenType = "TOK_LEFT_CURLY"     // {
+	TOK_RIGTH_CURLY    TokenType = "TOK_RIGTH_CURLY"    // }
+	TOK_PLUS_SIGN      TokenType = "TOK_PLUS_SIGN"      // +
+	TOK_MINUS_SIGN     TokenType = "TOK_MINUS_SIGN"     // -
+	TOK_STAR_SIGN      TokenType = "TOK_STAR_SIGN"      // *
+	TOK_LEFT_SQR_BCKT  TokenType = "TOK_LEFT_SQR_BCKT"  // [
+	TOK_RIGHT_SQR_BCKT TokenType = "TOK_RIGHT_SQR_BCKT" // ]
+	TOK_DOT            TokenType = "TOK_DOT"            // .
+	TOK_COMMA          TokenType = "TOK_COMMA"          // ,
+	TOK_CARET          TokenType = "TOK_CARET"          // ^
+	TOK_SLASH          TokenType = "TOK_SLASH"          // /
+	TOK_SEMICOLON      TokenType = "TOK_SEMICOLON"      // ;
+	TOK_QUESTION       TokenType = "TOK_QUESTION"       // ?
+	TOK_MOD_SIGN       TokenType = "TOK_MOD_SIGN"       // %
 )
 
 type Token struct {
 	Type   TokenType
 	Lexeme []rune
+	// The line number of the token
+	Line uint
 }
 
 func (token *Token) AsString() string {
-	return fmt.Sprintf("(Lexeme: %c, Type: %s)", token.Lexeme, token.Type)
+	return fmt.Sprintf("(Lexeme: %c, Type: %s, Line: %d)", token.Lexeme, token.Type, token.Line)
 }
